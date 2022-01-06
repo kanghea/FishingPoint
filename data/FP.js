fca85240361338c0066a6d4fdde4481b80518f47
kanghea <82922283+kanghea@users.noreply.github.com>
2022-01-06 16:39:18 +0900

    Create FP.js

---
+++ data/FP.js
@@ -0,0 +1,19 @@
+var Point = {
+    "searchResult": {
+         "Point": [
+              {
+                    "grd_lo": "128.74315019",
+                    "grd_la": "35.8072416"
+            },
+            {
+                    "grd_lo": "128.02906864",
+                    "grd_la": "35.52538081"
+            },
+            {
+                    "grd_lo": "128.44391383",
+                    "grd_la": "34.8643192"
+            }
+        ]
+    },
+    "resultCode": "Success"
+    }
